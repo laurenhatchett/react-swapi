48894067c6c71385133cffd3f7e170811df86bfd
laurenhatchett <laurenhatchett@gmail.com>
2022-10-13 19:32:11 -0400

    update Star Wars Lab

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { Route, Routes } from 'react-router-dom'
 // import NavBar from './components/NavBar/NavBar'
 import StarshipPage from './pages/StarshipPage/StarshipPage';
 import StarshipList from './pages/StarshipList/StarshipList';
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
   return (
     <div>
       <>
+      <NavBar/>
       <Routes>
         <Route
         path='/'
--- src/pages/StarshipPage/StarshipPage.jsx
+++ src/pages/StarshipPage/StarshipPage.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react"
 import { useLocation } from "react-router-dom";
 import { getStarshipDetails } from "../../services/sw-api";
-
+import { Link } from "react-router-dom";
 
 const StarshipPage = () => {
   const [starshipPage, setStarshipPage] = useState([])
@@ -18,13 +18,21 @@ const StarshipPage = () => {
   return (
     <>
       <div className="starship-details-container">
-        
+        {getStarshipDetails.name?
+        <>
         <h2>Name:{starshipPage.name}</h2>
         <h2>Model:{starshipPage.model}</h2>
-
+        <Link to='/'>
+      <p>Return</p> 
+        </Link>
+        </>
+        :
+        <>
+        <p>Loading Starship details...</p>
+        </>
+      }
       </div>
     </>
-
   );
 }
 
